ae9b4dde665349329e00a934f27684dc40db3bad
dzhalilov <dzhalilov.ilyas@gmail.com>
2022-04-05 13:08:59 +0300

    Enable h2 console

--- src/main/java/ru/javaops/topjava2/config/WebSecurityConfig.java
+++ src/main/java/ru/javaops/topjava2/config/WebSecurityConfig.java
@@ -55,5 +55,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().httpBasic()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().csrf().disable();
+        http.headers().frameOptions().sameOrigin(); // for enable h2 console
     }
 }
